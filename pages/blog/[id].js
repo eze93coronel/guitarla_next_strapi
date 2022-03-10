58cd41609b1b6ad9381e9b8dd3bce89a9156101d
roqueCoronel <rokecoronel748@gmail.com>
2022-03-10 12:55:39 -0300

    modificamos [dj],agregamos pag 404 mas estilos css con un modulo

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -1,13 +1,27 @@
-
-
+import Layout from "../../components/Layout";
+import Image from 'next/image'
+import {formatearFecha} from '../../helpers'
+import styles from '../../styles/Entrada.module.css'
+import { unstable_renderSubtreeIntoContainer } from "react-dom/cjs/react-dom.development";
 const EntradaBlog = ({entrada}) => {
 
-console.log(entrada);
+const {contenido,imagen,published_at,titulo}= entrada
   return (
-      <div>
-           <h1>desde entrADA BLOG</h1>
+    <Layout>
+      <main className="contenedor">
+        <h1 className="heading">{titulo}</h1>
+        <article className={styles.entrada}>
+          <Image layout='responsive' width={800} height={600} src={imagen.url} alt={`Imagen entrada ${titulo}`} />
+      <div className={styles.contenido}>
+        <p className={styles.fecha}>{formatearFecha(published_at)}</p>
+        <p className={styles.texto}>{contenido}</p>
+
       </div>
-   
+
+        </article>
+
+      </main>
+      </Layout>   
   )
 }
 export async function getStaticPaths(){
